9bfa5a468c2153db8d1d76526fb12318f2562159
haoyin <haoyin@uw.edu>
2021-10-05 17:38:22 -0700

    add license and change version number for python interfaces

--- py_interface/setup.py
+++ py_interface/setup.py
@@ -34,7 +34,7 @@ extension = setuptools.Extension("shm_pool",
                                  )
 
 setuptools.setup(name=name,
-                 version='0.0.2',
+                 version='1.0.0',
                  description="NS-3 AI interface for Python",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
