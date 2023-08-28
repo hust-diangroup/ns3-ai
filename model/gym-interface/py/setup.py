651440b19e6c446a9239cbae969b0882d2518736
muyuanshen <u202013816@hust.edu.cn>
2023-08-28 18:30:58 +0800

    add pure c++ test

--- model/gym-interface/py/setup.py
+++ model/gym-interface/py/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="ns3ai_gym_env",
     version="0.0.1",
-    install_requires=["numpy", "gymnasium", "protobuf"],
+    install_requires=["numpy", "gymnasium", "protobuf==3.20.3"],
 )
