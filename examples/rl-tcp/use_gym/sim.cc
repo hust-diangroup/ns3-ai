cb5c9de1d1112fdac8b433ce3ca0d8d6caeac841
muyuanshen <u202013816@hust.edu.cn>
2023-09-02 20:57:09 +0800

    comment benchmarking code that requires C++17 std::reduce

--- examples/rl-tcp/use_gym/sim.cc
+++ examples/rl-tcp/use_gym/sim.cc
@@ -52,15 +52,15 @@
 //extern std::vector<uint64_t> cpp2py_durations;
 //extern std::vector<uint64_t> py2cpp_durations;
 
-uint64_t average(std::vector<uint64_t> const& v){
-    if(v.empty()){
-        return 0;
-    }
-
-    auto count = v.size();
-
-    return std::reduce(v.begin(), v.end()) / count;
-}
+//uint64_t average(std::vector<uint64_t> const& v){
+//    if(v.empty()){
+//        return 0;
+//    }
+//
+//    auto count = v.size();
+//
+//    return std::reduce(v.begin(), v.end()) / count;
+//}
 
 using namespace ns3;
 
