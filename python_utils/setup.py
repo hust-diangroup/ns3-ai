1d6c46f9a85746338b4f4dbabbc91dc941ce31ec
muyuanshen <u202013816@hust.edu.cn>
2023-09-06 11:44:05 +0800

    copyright minor update

--- python_utils/setup.py
+++ python_utils/setup.py
@@ -1,5 +1,4 @@
-# -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
-# Copyright (c) 2019 Huazhong University of Science and Technology, Dian Group
+# Copyright (c) 2019-2023 Huazhong University of Science and Technology
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
